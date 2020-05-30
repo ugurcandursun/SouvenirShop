f9bccc28cab6bb48c446d89fd0be52247e11abe0
melikeakyuz <62993230+melikeakyuz@users.noreply.github.com>
2020-05-30 15:14:47 +0300

    Create admin-customer.component.ts

---
+++ src/app/component/admin-customer/admin-customer.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit, DebugElement} from '@angular/core';
+import { Router } from "@angular/router";
+import { HttpClient } from "@angular/common/http";
+
+@Component({
+  selector: 'app-admin-customer',
+  templateUrl: './admin-customer.component.html',
+  styleUrls: ['./admin-customer.component.css']
+})
+export class AdminCustomerComponent implements OnInit {
+  constructor(private router: Router, private http: HttpClient) {}
+  customers: any = [];
+ 
+  ngOnInit(): void {
+    this.http
+      .get("http://localhost:57367/api/customer")
+      .toPromise()
+      .then((element) => {
+        debugger;
+        this.customers = element;
+      });
+    
+  }
+  //insert
+  clickAdd($event,customer:any) {
+  // this.router.navigate(["/"]);
+  }
+//update
+clickUpdate($event,customer:any) {
+    localStorage.setItem('customerID',customer.CustomerID);
+  //  this.router.navigate(["/"]);
+ }
+//delete
+clickDelete($event,customer:any) {
+  
+  this.http.delete("http://localhost:57367/api/customer/"+customer.CustomerID)
+  .toPromise()
+      .then((element) => {
+        debugger;
+        this.router.navigate(["admin-customer"]);
+      });
+  
+}
+
+
+
+
+}
