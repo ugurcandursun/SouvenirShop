8f573bc52432611f726e965e6ccaeab02427af5c
melikeakyuz <62993230+melikeakyuz@users.noreply.github.com>
2020-06-01 18:18:34 +0300

    update admin.component

--- src/app/component/admin/admin.component.ts
+++ src/app/component/admin/admin.component.ts
@@ -9,25 +9,32 @@ import { HttpClient } from "@angular/common/http";
 })
 export class AdminComponent implements OnInit {
   constructor(private router: Router, private http: HttpClient) {}
-
+  all : any = [];
   customers: any = [];
   products: any = [];
+  sales: any = [];
   ngOnInit(): void {
     this.http
       .get("http://localhost:57367/api/user")
       .toPromise()
       .then((element) => {
-        
+        this.all=element;
         this.customers = element;
       });
+
       this.http
       .get("http://localhost:57367/api/product")
       .toPromise()
       .then((element) => {
-        
+        this.all=element;
         this.products = element;
       });
-      
+      this.http.get("http://localhost:57367/api/sales")
+      .toPromise()
+      .then((element) => {
+        this.all=element;
+        this.sales = element;
+      });
     }
-  
+    
 }
